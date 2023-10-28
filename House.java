33e9797567db97266c245715671c04b1b83d0956
rehabmalik <144557438+rehabmalik@users.noreply.github.com>
2023-10-27 20:35:38 -0400

    Update House.java

--- House.java
+++ House.java
@@ -1,12 +1,68 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
-    System.out.println("You have built a house: 🏠");
-  }
+public class House extends Building {
+    private ArrayList<String> residents;
+    private boolean hasDiningRoom;
+  
+   /**
+    * Creates a new house.
+    * @param name String
+    * @param address String
+    * @param nFloors int
+    * @param hasDiningRoom boolean
+    */
+    public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+     super(name,address,nFloors);
+     this.hasDiningRoom = hasDiningRoom;
+     this.residents = new ArrayList<>();
+     System.out.println("You have built a house: 🏠");
+    }
 
-  public static void main(String[] args) {
-    new House();
-  }
+   /**
+    * Indicates whether or not the house has a dining room.
+    * @return true if it does or false if it does not 
+    */
+    public boolean hasDiningRoom(){
+      return this.hasDiningRoom;
+    }
 
+    /**
+     * Indicates the number of residents in the house.
+     * @return number of residents
+     */
+    public int nResidents(){
+      return this.residents.size();
+    }
+    
+    /**
+     * Adds a new resident to the house
+     * @param name String
+     */
+    public void moveIn(String name){
+      this.residents.add(name);
+    }
+    
+    /**
+     * Removes a resident from the house
+     * @param name String
+     * @return name of the resident moved out
+     */
+    public String moveOut(String name){
+        this.residents.remove(name);
+        return name;
+    }
+
+    /**
+     * Checks whether or not a person is a resident of this house.
+     * @param person String
+     * @return true if the person is a resident or false if not 
+     */
+    public boolean isResident(String person){
+      if (this.residents.contains(person)){return true;}
+      else {return false;}
+    }
+
+    public static void main(String[] args) {
+      new House();
+    }
 }
